233dbdcbeca86c8ee44e8d91a353f7f1eaf28250
moldovanvasilica <mvie2572@scs.ubbcluj.ro>
2020-12-11 10:15:49 +0200

    final documentation

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ if __name__ == '__main__':
     grammar = Grammar("file.in")
     parser = Parser(grammar)
 
-    #print(parser.parse("individual a ; a = 2 ;"))
+    print(parser.parse("individual a ; a = 2 ;"))
+    print(parser.parse("a + ( a * a )"))
 
     #print(parser.parse("individual a ; individual b ; individual g ; come a ; come b ; parsing ( a != b ) { situation ( a > b ) { a = a - b ; } other { b = b - a ; } } g = a - 0 ; leave g ;"))
+    print(parser.create_parse_table())
 
     while(option!=0):
         option=int(input("What do you wanna see?"))
